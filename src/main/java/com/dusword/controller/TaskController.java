13a337e6bf557954d878f8f420f7d6385084cdec
lixiangjie <773983534@qq.com>
2021-05-05 20:36:58 +0800

    添加了删除task与对应predictedFile的接口

--- src/main/java/com/dusword/controller/TaskController.java
+++ src/main/java/com/dusword/controller/TaskController.java
@@ -2,13 +2,13 @@ package com.dusword.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.dusword.Mapper.PredictedFileMapper;
+import com.dusword.Mapper.TaskMapper;
 import com.dusword.Service.TaskService;
+import com.dusword.entity.PredictedFile;
 import com.dusword.entity.Task;
 import com.dusword.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -17,6 +17,10 @@ import javax.annotation.Resource;
 public class TaskController {
     @Resource
     TaskService taskService;
+    @Resource
+    TaskMapper taskMapper;
+    @Resource
+    PredictedFileMapper predictedFileMapper;
 
     @GetMapping("/findTaskList/{page}/{size}/{userId}")
     public Page<Task> findUserList(@PathVariable("page") Integer page, @PathVariable("size") Integer size,@PathVariable("userId") Integer userId){
@@ -24,5 +28,23 @@ public class TaskController {
         queryWrapper.eq("user_id",userId).orderByDesc("id");
         Page<Task> requestPage =new Page<>(page,size);
         return taskService.page(requestPage,queryWrapper);
+    }
+
+    @PostMapping("/deleteTask/{predictedFileId}")
+    public Integer deleteTask(@PathVariable("predictedFileId") Integer predictedFileId){
+//        System.out.println(predictedFileId);
+        QueryWrapper<Task> queryWrapper1 = new QueryWrapper<>();
+        queryWrapper1.eq("predicted_file_id",predictedFileId);
+        QueryWrapper<PredictedFile> queryWrapper2 = new QueryWrapper<>();
+        queryWrapper2.eq("id",predictedFileId);
+        int result1 = taskMapper.delete(queryWrapper1);
+        int result2 = predictedFileMapper.delete(queryWrapper2);
+//        System.out.println(result1);
+//        System.out.println(result2);
+        if (result1 ==1){
+            if (result2 ==1){
+                return  1;
+            }else return 0;
+        }else return 0;
     }
 }
